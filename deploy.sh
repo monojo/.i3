8d70ca1d8f286b72aa5035383281e2532a0fbd95
Xin <monojoker@gmail.com>
2019-03-16 02:43:01 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -d "$HOME/.i3/" ]; then
-	echo ".i3 exist"
-else
-	echo "no .i3 dir, creating it"
-	mkdir $HOME/.i3
-fi
+[ -f $HOME/.Xresources ] && rm -f $HOME/.Xresources
+    rm -f $HOME/.Xresources
 
-rm -f $HOME/.i3/config
-rm -f $HOME/.i3/i3status.conf
-rm -f $HOME/.Xresources
-
-ln -s $PWD/config $HOME/.i3/config
-ln -s $PWD/i3status.conf $HOME/.i3/i3status.conf
 ln -s $PWD/Xresources $HOME/.Xresources
